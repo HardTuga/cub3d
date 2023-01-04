2ec0ddc452b8e200dc111eecf15da2e8e234352e
Lucas filipe Martins da silva <lucas-ma@c1r20s2.42lisboa.com>
2023-01-04 14:33:24 +0000

    a

--- src/raycasting/ray_main.c
+++ src/raycasting/ray_main.c
@@ -6,20 +6,24 @@
 /*   By: lucas-ma <lucas-ma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 16:36:40 by lucas-ma          #+#    #+#             */
-/*   Updated: 2023/01/03 17:04:46 by lucas-ma         ###   ########.fr       */
+/*   Updated: 2023/01/04 14:05:39 by lucas-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "raycasting.h"
 
-double	*find_player(t_cub *map)
+t_vector	find_dir()
 {
-	
+	if 
 }
 
 void	ray_main(t_cub *map)
 {
-	double	p;
+	t_vector	p;
+	t_vector	dir;
+	t_vector	plane;
 
-	p = find_player(map);
+	p.x = (double) ;
+	p.y = (double) ;
+	dir = find_dir(map)
 }
--- src/raycasting/raycasting.h
+++ src/raycasting/raycasting.h
@@ -6,7 +6,7 @@
 /*   By: lucas-ma <lucas-ma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 11:52:43 by lucas-ma          #+#    #+#             */
-/*   Updated: 2022/12/29 12:56:32 by lucas-ma         ###   ########.fr       */
+/*   Updated: 2023/01/04 10:58:33 by lucas-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 # define RAYCASTING_H
 
 # include "../cub3d.h"
+
+typedef struct s_vector
+{
+	double	x;
+	double	y;
+}				t_vector;
+
 
 #endif
