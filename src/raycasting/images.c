048f8c74b18c57a28e60a7b6c65d503a9b38d45d
Lucas filipe Martins da silva <lucas-ma@c1r22s2.42lisboa.com>
2023-01-16 14:25:02 +0000

    ah

--- src/raycasting/images.c
+++ src/raycasting/images.c
@@ -6,7 +6,7 @@
 /*   By: lucas-ma <lucas-ma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 13:46:29 by lucas-ma          #+#    #+#             */
-/*   Updated: 2023/01/16 14:20:57 by lucas-ma         ###   ########.fr       */
+/*   Updated: 2023/01/16 14:24:52 by lucas-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	init_images(t_all *var)
 {
-	var->east = mlx_xpm_file_to_image(var->mlx.mlx ,var->cub->e, \
+	var->east.img = mlx_xpm_file_to_image(var->mlx.mlx ,var->cub->e, \
 	&var->tex_height, &var->tex_width);
-	
+	var->east.addr = mlx_get_data_addr(&var->east.img, );
 }
